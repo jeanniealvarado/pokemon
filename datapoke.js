5a81e3d0fabdda6620a9c1ac005cb6be026feef8
jeannie alvarado <jeaniie38@gmail.com>
2023-03-21 16:22:41 -0600

    mostrar data

--- datapoke.js
+++ datapoke.js
@@ -1,3 +1,5 @@
+
+let pokeCard = document.getElementById('pokemon')
 const pokemon = () => {
     fetch('https://pokeapi.co/api/v2/pokemon/ditto', {
         method: 'GET',
@@ -5,8 +7,16 @@ const pokemon = () => {
             'Content-type': 'application/json; charset=UTF-8',
         },
     })
-        .then((res) => Response.json(res))
-        .then((json => (console.log(json))))
+        .then((res) => res.json())
+        .then(data => {
+            pokeCard.innerText = JSON.stringify(data.name)
+            const experience = document.createElement('p')
+            experience.innerText = 'experiencia ' + JSON.stringify(data.base_experience)
+            pokeCard.appendChild(experience)
+            const pokeImg = document.createElement('img');
+            pokeImg.src = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/132.png"
+            pokeCard.appendChild(pokeImg)
+        })
 
 }
 pokemon()
